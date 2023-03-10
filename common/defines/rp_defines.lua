a283f9d369463c40cf7cfbfba038150794854b0d
ToMaToZz <krapi1201@gmail.com>
2023-03-10 21:12:19 +0100

    Pulled from RP Beta Mod

--- common/defines/rp_defines.lua
+++ common/defines/rp_defines.lua
@@ -5,57 +5,25 @@ NDefines.NDiplomacy.MIN_WARGOAL_JUSTIFY_COST = 6.0 --It always takes atleast 10 
 NDefines.NDiplomacy.VOLUNTEERS_PER_COUNTRY_ARMY = 0.05 --Each province owned by the target country contributes this amount of volunteers to the limit.
 NDefines.NDiplomacy.VOLUNTEERS_PER_TARGET_PROVINCE = 0.025 --Each army unit owned by the source country contributes this amount of volunteers to the limit.
 NDefines.NProduction.BASE_FACTORY_SPEED_NAV = 5 --VANILLA - 2.5
-NDefines.NAir.AIR_WING_ATTACK_LOGISTICS_NO_TRUCK_DISRUPTION_FACTOR = 0.02 -- If a unit isn't motorized, still disrupt its supply by damage * this
-NDefines.NAir.AIR_WING_ATTACK_LOGISTICS_TRUCK_DAMAGE_FACTOR = 0.15
-NDefines.NAir.AIR_WING_ATTACK_LOGISTICS_INFRA_DAMAGE_SPILL_FACTOR = 0.0006 -- Portion of truck damage to additionally deal to infrastructure
-NDefines.NAir.AIR_WING_ATTACK_LOGISTICS_TRAIN_DAMAGE_FACTOR = 0.025
-NDefines.NAir.AIR_WING_ATTACK_LOGISTICS_TRAIN_DAMAGE_DISRUPTION_MITIGATION = 1.5 -- Multiply Train Damage by (Smooth / (Smooth + (Disruption * Mitigation)))
-NDefines.NAir.AIR_WING_ATTACK_LOGISTICS_TRAIN_DAMAGE_DISRUPTION_SMOOTHING = 2.0
-NDefines.NAir.AIR_WING_ATTACK_LOGISTICS_RAILWAY_DAMAGE_SPILL_FACTOR = 0.003 -- Portion of train damage to additionally deal to railways
-NDefines.NAir.AIR_WING_ATTACK_LOGISTICS_DISRUPTION_MIN_DAMAGE_FACTOR = 0.05 -- Multiply train damage by this factor, scale from 1.0 at 0 disruption to this at AIR_WING_ATTACK_LOGISTICS_MAX_DISRUPTION_DAMAGE_TO_CONSIDER
-NDefines.NAir.AIR_WING_ATTACK_LOGISTICS_MAX_DISRUPTION_DAMAGE_TO_CONSIDER = 5.0 -- see above
-NDefines.NAir.AIR_WING_ATTACK_LOGISTICS_DIRECT_DISRUPTION_DAMAGE_FACTOR = 0.01 -- Disruption damage to supply throughput done by bombing damage, not dependant on killing trains which also causes diruption.
-NDefines.NAir.AIR_WING_ATTACK_LOGISTICS_TRUCK_MAX_FACTOR = 0.05 -- max trucks we can destroy in one instance of a logistics strike
-NDefines.NAir.MISSION_COMMAND_POWER_COSTS = {  -- command power cost per plane to create a mission
-		0.0, -- AIR_SUPERIORITY
-		0.0, -- CAS
-		0.0, -- INTERCEPTION
-		0.0, -- STRATEGIC_BOMBER
-		0.0, -- NAVAL_BOMBER
-		0.0, -- DROP_NUKE
-		0.0, -- PARADROP
-		0.0, -- NAVAL_KAMIKAZE
-        0.0, -- PORT_STRIKE
-		0.5, -- ATTACK_LOGISTICS
-		0.2, -- AIR_SUPPLY
-		0.0, -- TRAINING
-		0.0, -- NAVAL_MINES_PLANTING
-		0.0, -- NAVAL_MINES_SWEEPING
-		0.0, -- MISSION_RECON
-	}
-NDefines.NAir.MISSION_FUEL_COSTS = {  -- fuel cost per plane for each mission
-		1.0, -- AIR_SUPERIORITY
-		1.0, -- CAS
-		0.2, -- INTERCEPTION
-		1.0, -- STRATEGIC_BOMBER
-		1.0, -- NAVAL_BOMBER
-		1.0, -- DROP_NUKE
-		1.0, -- PARADROP
-		0.75, -- NAVAL_KAMIKAZE
-		1.2, -- PORT_STRIKE
-		1.5, -- ATTACK_LOGISTICS
-		1.0, -- AIR_SUPPLY
-		0.6, -- TRAINING
-		1.0, -- NAVAL_MINES_PLANTING
-		1.0, -- NAVAL_MINES_SWEEPING
-		1.0, -- MISSION_RECON
-	}
-<<<<<<< Updated upstream
-NDefines.NAir.AIR_WING_MAX_SIZE = 100 -- Max amount of airplanes in wing
-=======
-NDefines.NAir.AIR_WING_MAX_SIZE	= 100 --Max amount of airplanes in wing
-
->>>>>>> Stashed changes
 NDefines.NSupply.FLOATING_HARBOR_BASE_SUPPLY = 10.0
 NDefines.NSupply.FLOATING_HARBOR_BASE_DURATION = 14
 
+NDefines.NAir.MISSION_COMMAND_POWER_COSTS = { -- command power cost per plane to create a mission
+			0.0, -- AIR_SUPERIORITY
+			0.0, -- CAS
+			0.0, -- INTERCEPTION
+			0.0, -- STRATEGIC_BOMBER
+			0.0, -- NAVAL_BOMBER
+			0.0, -- DROP_NUKE
+			0.0, -- PARADROP
+			0.0, -- NAVAL_KAMIKAZE
+			0.0, -- PORT_STRIKE
+			0.055, -- ATTACK_LOGISTICS
+			0.025, -- AIR_SUPPLY --Was 0.05
+			0.0, -- TRAINING
+			0.0, -- NAVAL_MINES_PLANTING
+			0.0, -- NAVAL_MINES_SWEEPING
+			0.0, -- MISSION_RECON
+			0.0, -- NAVAL_PATROL
+		}
+NDefines.NDiplomacy.GUARANTEE_COST = 100 
