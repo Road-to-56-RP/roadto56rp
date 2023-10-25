b65fd5cfcd3c2372ec13eb7188f3793303bbb2fe
Warlider <48133480+Warlider@users.noreply.github.com>
2023-10-25 17:08:13 +0200

    25/10/2023 Update
    MILITARY:
    - Torpedo tubes 1 and submarine engine’s 1, 2, 3 and 4 will be equipped for Sub’s 1, 2, 3 and 4 respectively by default.
    - Submarine 3 and 4’s will be able to use the rear slot for an auxiliary module as well as torpedoes (radar / snorkel)
    - Submarine 5’s received an additional auxiliary slot.
    - Added a limit of 1 snorkel per submarine.
    
    RESEARCH:
    - Cruiser and Midget subs are now researchable for everyone.
    
    MIO’s
    - Fun sekrit addition.
    - Renamed "Liaotung Arsenal" to "Dowa Automotive". [ As far as i can tell, "Liaotung Arsenal" never existed, feel free to prove me wrong - Warlider ]
    - “Dowa Automotive” requires the “Liaotung” state to be controlled by the owner to be used, instead of “Liaoning”
    - Renamed "Mukden Arsenal" to the Japanese-used name "Fengtian Ordanance Manufacturing"
    - Added custom icons for “Fengtian Ordanance Manufacturing”, “Hanyan Arsenal”, “Dowa Automotive” and “Taiyuan Arsenal”.
    
    OTHER:
    - Upped the pause and slowdown timers to 90 and 30 days respectively.
    - Game rules will now pick the R56 focus tree’s for Finland, Sweden, Norway and Denmark.
    - Keybindings changed for RP Tools:
    1. RP Tools keybind now “ctrl+shift+q”
    2. RTT now “ctrl+shift+w”
    3. STT now “ctrl+shift+e”
    4. HTT now “ctrl+shift+r”
    5. “Mark State” type of action now universally under “ctrl+c”
    6. “Give State” type of action universally under “ctrl+v”
    7. “Clear All Marks” type of action universally under “ctrl+z”
    
    
    
    **NATION SPECIFIC:**
    ***TAG:***
    USA:
    - Fixed Naval Maritime Patrol Bombers design not appearing on game start as they should.
    GER:
    - Ludendorff now does exist in-game.
    
    
    Commits:
    
    * Update r56rp_formables_interface_l_english.yml
    * Delete r56rp_POR_l_english.txt
    * Project Orion
    Fuck knows what i did, i was half-dead while coding most of it.
    * Plant 37 picture
    * Ludendorf Fix
    * Operation Floatation
    - Cruiser and Midget subs now a normal researchable tech.
    - Made super heavy ships a separate research again.
    - Buffed the range on nuclear subs.
    - Added GFX for midget subs.
    - Finalized transitioning the Host tool to the RP Tools window.
    - New key binds for the RP Tool.
    - Fixed US not getting a Maritime Patrol design. (turns out, you gotta treat the frame based on the equipment type, not the overall frame.)
    * ah fuck
    * Operation Manchu
    - Added custom icons for some chinese manufacturers.
    - Renamed "Liaotung Arsenal" to "Dowa Automotive". As far as i can tell, "Liaotung Arsenal" never existed.
    - Renamed "Mukden Arsenal" to the Japanese-used name "Fengtian Ordinance Manufacturing"
    - Upped the pause and slowdown timers to 90 and 30 days respectivley.
    * Forgor Mitsubishi here
    
    ---------
    
    Co-authored-by: SimpyRT56RP <106488401+SimpyRT56RP@users.noreply.github.com>

--- common/defines/rp_defines.lua
+++ common/defines/rp_defines.lua
@@ -1,6 +1,6 @@
 -- THUNDERDOME speed defines by PIZZA DO NOT MOVE ANYWHERE ELSE FUCKS WITH EVERYTHING
--- NDefines.NGame.LAG_DAYS_FOR_LOWER_SPEED = 1000 -- Lag days
--- NDefines.NGame.LAG_DAYS_FOR_PAUSE = 1000 -- pause days
+NDefines.NGame.LAG_DAYS_FOR_LOWER_SPEED = 30 -- Lag days
+NDefines.NGame.LAG_DAYS_FOR_PAUSE = 90 -- pause days
 -- NDefines.NGame.GAME_SPEED_SECONDS = { 2.0, 0.10, 0.05, 0.035, 0 } -- game secs
 
 NDefines.NCountry.ARMY_COUNT_DAILY_DECREASE_FOR_TRAINING_XP = 0.05 -- number of armies that is used in training xp calculates daily linearly approaches this number (if real number is lower)
